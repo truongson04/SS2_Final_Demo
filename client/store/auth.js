fa48167332e01e5b4e92b8726205762276943d61
baodeptrai912 <dangson2376@gmail.com>
2026-02-06 04:53:20 +0700

    change

---
+++ client/store/auth.js
@@ -0,0 +1,12 @@
+import { defineStore } from "pinia";
+import { ref } from "vue";
+
+const useAuth = defineStore("auth", () => {
+  const token = ref(null);
+  const user = ref(null);
+  const loading = ref(true);
+  const login = (token, user) => {
+    token.value = token;
+    user.value = user;
+  };
+});
--- server/controllers/aiControllers.js
+++ server/controllers/aiControllers.js
@@ -1,21 +1,119 @@
 import genAI from "../config/ai.js";
+import Resume from "../models/Resumes.js";
 
-export const enhanceProfessionalSummary = async(req, res)=>{
-try {
-    const {userContent}= req.body;
-    if(!userContent){
-        return res.status(400).json({message:"Missing fields"})
+export const enhanceProfessionalSummary = async (req, res) => {
+  try {
+    const { userContent } = req.body;
+    if (!userContent) {
+      return res.status(400).json({ message: "Missing fields" });
     }
     const model = genAI.getGenerativeModel({
-        model:process.env.GEMINI_MODEL,
-        systemInstruction:"You are an expert in resume writing. Your task is to enhance the professional summary of a resume. The summary should be 1-2 sentences also highlighting key skills, experience, and career objectives. Make it compelling and ATS-friendly. and only return text no options or anything else."
-    })
+      model: process.env.GEMINI_MODEL,
+      systemInstruction:
+        "You are an expert in resume writing. Your task is to enhance the professional summary of a resume. The summary should be 1-2 sentences also highlighting key skills, experience, and career objectives. Make it compelling and ATS-friendly. and only return text no options or anything else.",
+    });
     const result = await model.generateContent(userContent);
     const response = await result.response;
-    const enhanceContent = response.text()
-    return res.status(200).json({enhanceContent})
-
-} catch (error) {
-    return res.status(400).json({message:error.message})
-}
-}
+    const enhanceContent = response.text();
+    return res.status(200).json({ enhanceContent });
+  } catch (error) {
+    return res.status(400).json({ message: error.message });
+  }
+};
+export const enhanceJobDescription = async (req, res) => {
+  try {
+    const { userContent } = req.body;
+    if (!userContent) {
+      return res.status(400).json({ message: "Missing required fields" });
+    }
+    const model = genAI.getGenerativeModel({
+      model: process.env.GEMINI_MODEL,
+      systemInstruction:
+        "You are an expert in resume writing. Your task is to enhance'the job description of a resume. The job description should be only in 1-2 sentence also highlighting key responsibilities and achievements. Use action verbs and quantifiable results where possible. Make it ATS-friendly. and only return text no options or anything else.",
+    });
+    const result = await model.generateContent(userContent);
+    const response = await result.response;
+    const enhanceContent = response.text();
+    return res.status(200).json({ enhanceContent });
+  } catch (error) {
+    return res.status(400).json({ message: error.message });
+  }
+};
+export const uploadResume = async (req, res) => {
+  try {
+    const { resumeText, title } = req.body;
+    const userId = req.userId;
+    if (!resumeText) {
+      return res.status(400).json({ message: "Missing required fields" });
+    }
+    const systemPrompt = `You are an expert AI Agent to extract data from this resume : ${resumeText} Provide data in this following JSON format with no additional text before or after: {
+professional_summary:{
+        type:String,
+        default:''
+    },
+    skills:[{
+        type:String
+    }],
+    personal_info:{
+        image:{type:String, default:''},
+        full_name:{type:String, default:''},
+        profession:{type:String, default:""},
+        email:{type:String, default:''},
+        phone:{type:String, default:""},
+        linkedin:{type:String, default:''},
+        website:{type:String, default:''}
+    },
+    experience:[
+        {
+            company:{type:String},
+            position:{type:String},
+            start_date:{type:String},
+            end_data:{type:String},
+            description:{type:String},
+            is_current:{type:Boolean}
+        }
+    ],
+    project:[
+        {
+            name:{type:String},
+            type:{type:String},
+            description:{type:String}
+        }
+    ],
+    education:[
+        {
+            institution:{type:String},
+            degree:{type:String},
+            field:{type:String}, 
+             graduation_date:{type:String}, 
+              gpa:{type:String}, 
+              
+        }
+    ]}`;
+    const userPrompt = "extract data from this resume";
+    const model = genAI.getGenerativeModel({
+      model: process.env.GEMINI_MODEL,
+      systemInstruction: systemPrompt,
+    });
+    const result = await model.generateContent({
+      contents: [
+        {
+          role: "user",
+          parts: [
+            {
+              text: userPrompt,
+            },
+          ],
+          generationConfig: {
+            responseMimeType: "application/json",
+          },
+        },
+      ],
+    });
+    const response = JSON.parse(result.response.text());
+    const newResume = await Resume.create({ userId, title, ...response });
+    res.json({ resumeId: newResume._id });
+  } catch (error) {
+    return res.status(400).json({ message: error.message });
+  }
+};
