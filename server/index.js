f92f74a063a62244423891da68d4580d521c4876
baodeptrai912 <dangson2376@gmail.com>
2026-02-13 22:57:03 +0700

    Final version change

--- server/index.js
+++ server/index.js
@@ -12,7 +12,8 @@ import aiRouter from "./routes/aiRoute.js";
 import { googleResister } from "./controllers/userControllers.js";
 
 const app = express();
-app.use(express.json());
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
 
 // connect to db
 app.use(async (req, res, next) => {
